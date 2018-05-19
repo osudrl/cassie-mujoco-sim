5d7d7c1d5d21be06aff37ccca39ec92b0033ca61
Patrick Clary <pclary@outlook.com>
2018-05-19 14:28:06 -0700

    Renamed external force function in python

--- example/cassiemujoco.py
+++ example/cassiemujoco.py
@@ -83,7 +83,7 @@ class CassieSim:
     def release(self):
         cassie_sim_release(self.c)
 
-    def force(self, xfrc, body=1):
+    def apply_force(self, xfrc, body=1):
         xfrc_array = (ctypes.c_double * 6)()
         for i in range(len(xfrc)):
             xfrc_array[i] = xfrc[i]
